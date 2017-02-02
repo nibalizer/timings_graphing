c6754c5b78f1c3596fb99cadd94ef62a92d114c0
Spencer Krum <nibz@spencerkrum.com>
2017-02-02 11:35:33 -0800

    Use a class, so we can use jupyter

--- timings.py
+++ timings.py
@@ -13,18 +13,20 @@ def frame_to_time(frame):
     seconds = game_seconds - (60 * minutes)
     return '{0}:{1:02d}'.format(minutes, seconds)
 
+
 class TimingsTracker():
-    def __init__(self):
+    def __init__(self, sup):
         self.name = "TimingsTracker"
+        self.sup = sup
 
     def handleEvent(self, event, replay):
         pass
 
     def handleUnitTypeChangeEvent(self, event, replay):
         if event.unit.name == "Lair":
-            print "Lair Created"
-            print event.frame, frame_to_time(event.frame)
-            lair_timings.append(event.frame)
+            # print "Lair Created"
+            # print event.frame, frame_to_time(event.frame)
+            self.sup.lair_timings.append(event.frame)
 
     def handleUnitDoneEvent(self, event, replay):
         # print event.name
@@ -33,14 +35,22 @@ class TimingsTracker():
             # if event.frame != 0:
             f = [i for i in event.unit.owner.units if "Command" in str(i)]
             if len(f) < 3:
-                print "Command Center Created"
-                print event.frame, frame_to_time(event.frame)
-                command_center_timings.append(event.frame)
+                # print "Command Center Created"
+                # print event.frame, frame_to_time(event.frame)
+                self.sup.command_center_timings.append(event.frame)
+
+        if str(event.unit).startswith("Nexus"):
+            # if event.frame != 0:
+            f = [i for i in event.unit.owner.units if "Nexus" in str(i)]
+            if len(f) == 3:
+                # print "Third Nexus Created, (", f, ")"
+                # print event.frame, frame_to_time(event.frame)
+                self.sup.third_nexus_timings.append(event.frame)
 
         if str(event.unit).startswith("SpawningPool"):
-            print "SpawningPool Created"
-            print event.frame, frame_to_time(event.frame)
-            spawning_pool_timings.append(event.frame)
+            # print "SpawningPool Created"
+            # print event.frame, frame_to_time(event.frame)
+            self.sup.spawning_pool_timings.append(event.frame)
 
     def handleControlGroupEvent(self, event, replay):
         pass
@@ -49,39 +59,45 @@ class TimingsTracker():
         pass
 
 
-if __name__ == "__main__":
-    debug = False
-    command_center_timings = []
-    spawning_pool_timings = []
-    lair_timings = []
+class Analyzer():
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.command_center_timings = []
+        self.spawning_pool_timings = []
+        self.lair_timings = []
+        self.third_nexus_timings = []
 
-    # find all replay files in 'replays' dir and add theem to an array
-    replay_files = []
-    error_replays = []
-    for root, dirs, files in os.walk("replays"):
-        path = root.split('/')
-        for file in files:
-            if file.endswith(".SC2Replay"):
-                replay_files.append(root + "/" + file)
-    if debug:
-        print replay_files
+        # find all replay files in 'replays' dir and add theem to an array
+        self.replay_files = []
+        self.error_replays = []
+        for root, dirs, files in os.walk("replays"):
+            path = root.split('/')
+            for file in files:
+                if file.endswith(".SC2Replay"):
+                    self.replay_files.append(root + "/" + file)
+        if debug:
+            print self.replay_files
 
-    try:
-        for file in replay_files:
-            replay = sc2reader.load_replay(
-                file,
-                engine=sc2reader.engine.GameEngine(plugins=[
-                    ContextLoader(),
-                    APMTracker(),
-                    TimingsTracker(),
-                    SelectionTracker(),
-                ])
-            )
-    except:
-        error_replays.append(file)
+    def process(self):
+        try:
+            for file in self.replay_files:
+                replay = sc2reader.load_replay(
+                    file,
+                    engine=sc2reader.engine.GameEngine(plugins=[
+                        ContextLoader(),
+                        APMTracker(),
+                        TimingsTracker(self),
+                        SelectionTracker(),
+                    ])
+                )
+        except:
+            self.error_replays.append(file)
 
-    print "CC timings", command_center_timings
-    print "pool timings", spawning_pool_timings
-    print "lair timings", lair_timings
-    if debug:
-        print "Error replays found in: ", error_replays
+    def print_output(self):
+
+        print "CC timings", self.command_center_timings
+        print "pool timings", self.spawning_pool_timings
+        print "lair timings", self.lair_timings
+        print "third nexus timings", self.third_nexus_timings
+        if self.debug:
+            print "Error replays found in: ", self.error_replays
